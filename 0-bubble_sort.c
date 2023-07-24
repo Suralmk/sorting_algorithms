28854b47ddfbae1d32d944058359b67c8233acdb
Suralmk <104755355+Suralmk@users.noreply.github.com>
2023-07-24 11:55:13 +0300

    new

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap_ints - this func swap two numbers
+ * @firstnum: first number
+ * @secondnum: second number
+ */
+void swap_ints(int *firstnum, int *secondnum)
+{
+	int temp;
+
+	temp = *firstnum;
+	*firstnum = *secondnum;
+	*secondnum = temp;
+}
+
+/**
  * bubble_sort - sort an array of integers
  * @array: an array to be sorted
  * @size: size of array
