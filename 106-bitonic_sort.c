6a58b93aa70557527d068949e35661204e739a5c
Suralmk <104755355+Suralmk@users.noreply.github.com>
2023-07-25 13:06:25 +0300

    biotonic

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 
-void change_int(int *a, int *b);
+void swap_ints(int *a, int *b);
 void merge_bio(int *array, size_t size, size_t begin, size_t sequence,
 		char direction);
 void sequence_bio(int *array, size_t size, size_t begin, size_t sequence, char direction);
@@ -9,8 +9,8 @@ void bitonic_sort(int *array, size_t size);
 /**
  * bitonic_sort - Sort an array of integers in ascending
  *                order using the bitonic sort algorithm.
- * @array: An array
- * @size: The size
+ * @array: An array of integers.
+ * @size: The size of the array.
  */
 void bitonic_sort(int *array, size_t size)
 {
@@ -19,6 +19,7 @@ void bitonic_sort(int *array, size_t size)
 
 	sequence_bio(array, size, 0, size, UP);
 }
+
 /**
  * change_int - this func swap two numbers
  * @firstnum: first number
@@ -37,41 +38,41 @@ void change_int(int *firstnum, int *secondnum)
  * merge_bio - Sort a bitonic sequenceuence inside an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
- * @begin: The begining index of the sequenceuence in array to sort.
- * @sequence: The size of the sequenceuence to sort.
+ * @begin: The begining index.
+ * @sequence: The size of the sequence to sort.
  * @direction: The direction to sort in.
  */
 void merge_bio(int *array, size_t size, size_t begin, size_t sequence,
 		char direction)
 {
-	size_t i, jump = sequence / 2;
+	size_t i, wide = sequence / 2;
 
 	if (sequence > 1)
 	{
-		i = begin;
-		while (i < begin + jump)
+		i = begin; 
+		while (i < begin + wide)
 		{
-			if ((direction == UP && array[i] > array[i + jump]) ||
-			    (direction == DOWN && array[i] < array[i + jump]))
-				change_int(array + i, array + i + jump);
+			if ((direction == UP && array[i] > array[i + wide]) ||
+			    (direction == DOWN && array[i] < array[i + wide]))
+				swap_ints(array + i, array + i + wide);
+				i++;
 		}
-		merge_bio(array, size, begin, jump, direction);
-		merge_bio(array, size, begin + jump, jump, direction);
-		i++;
+		merge_bio(array, size, begin, wide, direction);
+		merge_bio(array, size, begin + wide, wide, direction);
 	}
 }
 
 /**
- * sequence_bio - convert array to biotonic.
+ * sequence_bio - Convert an array of integers into a bitonic sequenceuence.
  * @array: An array of integers.
  * @size: The size of the array.
  * @begin: The begining index of a block of the building bitonic sequenceuence.
- * @sequence: The size of a block of the building bitonic sequence.
- * @direction: The direction to sort the bitonic
+ * @sequence: The size of a block of the building bitonic sequenceuence.
+ * @direction: The direction to sort the bitonic sequenceuence block in.
  */
 void sequence_bio(int *array, size_t size, size_t begin, size_t sequence, char direction)
 {
-	size_t cut = sequence / 2;
+	size_t drop = sequence / 2;
 	char *str = (direction == UP) ? "UP" : "DOWN";
 
 	if (sequence > 1)
@@ -79,8 +80,8 @@ void sequence_bio(int *array, size_t size, size_t begin, size_t sequence, char d
 		printf("Merging [%lu/%lu] (%s):\n", sequence, size, str);
 		print_array(array + begin, sequence);
 
-		sequence_bio(array, size, begin, cut, UP);
-		sequence_bio(array, size, begin + cut, cut, DOWN);
+		sequence_bio(array, size, begin, drop, UP);
+		sequence_bio(array, size, begin + drop, drop, DOWN);
 		merge_bio(array, size, begin, sequence, direction);
 
 		printf("Result [%lu/%lu] (%s):\n", sequence, size, str);
